c2ae4f49ee4a3431c4037d8ed8b9b9fa3c63dbb0
Fadify <86985819+Fadify@users.noreply.github.com>
2021-08-20 23:18:08 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 7081641016 do
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/BankRobSim.lua", true))()
-elseif
+else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/NotSupport.lua", true))()
   end
   
