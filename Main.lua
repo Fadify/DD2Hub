c2cafe3ba94f4d57e77e8fa44b03e76a5c48cdbe
Fadify <86985819+Fadify@users.noreply.github.com>
2021-08-20 23:22:22 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,9 @@
-if game.PlaceId == 7081641016 do
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/BankRobSim.lua", true))()
-elseif
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/NotSupport.lua", true))()
-  end
-  
+if game.PlaceId == 7081641016 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/BankRobSim.lua")()
+        )
+
+elseif  loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/NotSupport.lua"))()
+ then
+     warn("suussy baka")
+end
   
