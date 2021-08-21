3a004d3425c0599210a45dca74826218894ee953
Fadify <86985819+Fadify@users.noreply.github.com>
2021-08-21 03:39:43 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,6 @@
 if game.PlaceId == 7081641016 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/BankRobSim.lua"))()
-elseif  loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/NotSupport.lua"))() then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/BankRobSim.lua", true))()
+elseif game.PlaceId == 3956818381 then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/NinjaLegends.lua", true))()
+elseif  loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/NotSupport.lua", true))() then
    end
